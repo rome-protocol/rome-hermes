a6a9265db86135ea43f121d480f3624cca166369
ldback-aftermath <mck@aftermath.finance>
2025-01-21 16:35:21 +0600

    fix!: remove redundant `&safe` in `TransactionData` decode function

--- crates/af-sui-types/src/sui/transaction/data.rs
+++ crates/af-sui-types/src/sui/transaction/data.rs
@@ -66,7 +66,6 @@ impl TransactionData {
     /// Deserialize a transaction from base64 bytes.
     #[deprecated = "Unnecessary &self; will be removed with the next breaking change"]
     pub fn decode_base64(
-        &self,
         value: impl AsRef<[u8]>,
     ) -> Result<Self, TransactionFromBase64Error> {
         Ok(bcs::from_bytes(&decode_base64_default(value).map_err(
