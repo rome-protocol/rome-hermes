c06ca8d83d090ed6d310ece61f2d3841d66897ab
unmaykr (aftermath) <98741738+unmaykr-aftermath@users.noreply.github.com>
2025-01-27 07:27:24 -0300

    test: derive MoveStruct for braced struct
    First `trybuild` test for `af-move-type-derive`

---
+++ crates/af-move-type-derive/tests/braced_struct.rs
@@ -0,0 +1,16 @@
+use af_move_type::MoveStruct;
+use serde::{Deserialize, Serialize};
+
+#[derive(MoveStruct, Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq, Hash)]
+#[move_(nameless)]
+pub struct Otw {
+    dummy_field: bool,
+}
+
+impl std::fmt::Display for Otw {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "OTW")
+    }
+}
+
+fn main() {}
