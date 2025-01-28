0517bc0c4a04c6d57bdf75c6522bacf37722eb86
unmaykr (aftermath) <98741738+unmaykr-aftermath@users.noreply.github.com>
2025-01-28 07:08:06 -0300

    refactor: generalize queries::stream::forward a bit
    - `Iterator` -> `IntoIterator`
    - Simplify lifetimes

--- crates/sui-gql-client/src/queries/stream/mod.rs
+++ crates/sui-gql-client/src/queries/stream/mod.rs
@@ -15,20 +15,19 @@ use crate::GraphQlClient;
 /// - `client`
 /// - `vars`: [`cynic::QueryVariables`] fragment; must implement [`UpdatePageInfo`]
 /// - `request`: async function that maps `(client, vars) -> Page<Iter>`, where `Iter` is an
-///   [`Iterator`] over items of a **single** page's results
-pub(super) fn forward<'a, 'b, Client, Vars, Req, Fut, Iter, T>(
+///   iterator over items of a **single** page's results
+pub(super) fn forward<'a, Client, Vars, Req, Fut, Iter, T>(
     client: &'a Client,
     mut vars: Vars,
     mut request: Req,
 ) -> impl Stream<Item = Result<T, Client>> + 'a
 where
     Client: GraphQlClient,
-    Vars: 'b + UpdatePageInfo + Clone,
-    Req: 'b + FnMut(&'a Client, Vars) -> Fut,
+    Vars: 'a + UpdatePageInfo + Clone,
+    Req: 'a + FnMut(&'a Client, Vars) -> Fut,
     Fut: 'a + Future<Output = Result<Page<Iter>, Client>>,
-    Iter: Iterator<Item = Result<T, Client>>,
+    Iter: IntoIterator<Item = Result<T, Client>>,
     T: 'static,
-    'b: 'a,
 {
     async_stream::try_stream! {
         let mut has_next_page = true;
