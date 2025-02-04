4a05cb996ae6a47ada725a63a341e1721f83801b
unmaykr (aftermath) <98741738+unmaykr-aftermath@users.noreply.github.com>
2025-02-04 15:54:53 -0300

    chore: ensure GraphQL schema is up-to-date
    With `testnet-v1.42.0`

--- crates/sui-gql-schema/src/lib.rs
+++ crates/sui-gql-schema/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(all(doc, not(doctest)), feature(doc_auto_cfg))]
 
-//! Sui's GraphQL schema to use with `cynic`. Synchronized at `testnet-v1.40.2.`
+//! Sui's GraphQL schema to use with `cynic`. Synchronized at `testnet-v1.42.0.`
 //!
 //! Read more about schemas in <https://cynic-rs.dev/schemas> and why this crate is separate in
 //! <https://cynic-rs.dev/large-apis>.
