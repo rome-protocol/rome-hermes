6ef57a1788c4447aa6c88aa229927845df3ddc88
admin-aftermath <github@aftermath.finance>
2025-02-17 16:05:14 -0800

    chore: `cargo fmt`

--- crates/sui-gql-client/examples/owner_latest_dof_content.rs
+++ crates/sui-gql-client/examples/owner_latest_dof_content.rs
@@ -1,11 +1,12 @@
-use color_eyre::Result;
 use af_sui_types::TypeTag;
+use color_eyre::Result;
 use sui_gql_client::queries::outputs::RawMoveValue;
 use sui_gql_client::queries::GraphQlClientExt as _;
 use sui_gql_client::reqwest::ReqwestClient;
 
 const SUI_GRAPHQL_SERVER_URL: &str = "https://sui-testnet.mystenlabs.com/graphql";
-const STAKED_SUI_VAULT_OBJECT_ID: &str = "0xe498a8c07ec62200c519a0092eda233abdab879e8f332c11bdc1819eb7b12fbb";
+const STAKED_SUI_VAULT_OBJECT_ID: &str =
+    "0xe498a8c07ec62200c519a0092eda233abdab879e8f332c11bdc1819eb7b12fbb";
 const VERSION: u64 = 0;
 
 #[tokio::main]
