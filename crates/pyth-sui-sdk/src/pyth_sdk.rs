7dd9d96f21d235c7461faf6368e790b4c3f9edc2
matteo-aftermath <matteo@aftermath.finance>
2025-03-25 13:58:40 +0100

    feat(pyth-sui-sdk): add get_pyth_price

--- crates/pyth-sui-sdk/src/pyth_sdk.rs
+++ crates/pyth-sui-sdk/src/pyth_sdk.rs
@@ -1,3 +1,4 @@
+use af_utilities::IFixed;
 use pyth_sdk::PriceIdentifier;
 
 use crate::price_info::PriceInfoObject;
@@ -15,4 +16,29 @@ impl PriceInfoObject {
                 .expect("Validated lenght onchain"),
         )
     }
+
+    pub fn get_pyth_price(&self) -> Result<IFixed, Error> {
+        let pyth_price = &self.price_info.price_feed.price;
+        let price = if pyth_price.price.negative {
+            return Err(Error::NegativePythPrice(pyth_price.price.magnitude as i64));
+        } else if pyth_price.expo.negative {
+            IFixed::from(pyth_price.price.magnitude)
+                / IFixed::from(u64::pow(10, pyth_price.expo.magnitude as u32))
+        } else {
+            IFixed::from(pyth_price.price.magnitude)
+                * IFixed::from(u64::pow(10, pyth_price.expo.magnitude as u32))
+        };
+
+        Ok(price)
+    }
+
+    pub const fn get_timestamp_ms(&self) -> u64 {
+        self.price_info.price_feed.price.timestamp * 1000
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Pyth price is negative: -{0}")]
+    NegativePythPrice(i64),
 }
