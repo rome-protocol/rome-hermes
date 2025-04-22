b474d43ff49a5203c583e0e300d8a47e30399fdc
unmaykr (aftermath) <98741738+unmaykr-aftermath@users.noreply.github.com>
2025-04-22 16:41:14 +0400

    refactor(af-iperps): use `trait-variant` internally

--- crates/af-iperps/src/graphql/mod.rs
+++ crates/af-iperps/src/graphql/mod.rs
@@ -1,9 +1,8 @@
-use std::future::Future;
-
 use af_move_type::MoveInstance;
 use af_sui_types::{Address, ObjectId, Version};
 use futures::Stream;
 use sui_gql_client::GraphQlClient;
+pub use sui_gql_client::queries::Error;
 
 use crate::Vault;
 use crate::orderbook::Order;
@@ -15,12 +14,13 @@ mod ch_vault;
 mod map_orders;
 mod order_maps;
 
-pub use sui_gql_client::queries::Error;
-
 pub use self::ch_vault::Error as ChVaultError;
 pub use self::order_maps::OrderMaps;
 
+type Result<T, C> = ::std::result::Result<T, Error<<C as GraphQlClient>::Error>>;
+
 /// Extension trait to [`GraphQlClient`] collecting all defined queries in one place.
+#[trait_variant::make(Send)]
 pub trait GraphQlClientExt: GraphQlClient + Sized {
     /// Snapshot of the orders on one side of the orderbook, rooted at the [`ClearingHouse`] id
     /// and version.
@@ -37,7 +37,7 @@ pub trait GraphQlClientExt: GraphQlClient + Sized {
         ch: ObjectId,
         version: Option<Version>,
         asks: bool,
-    ) -> impl Stream<Item = Result<(u128, Order), Error<Self::Error>>> + '_ {
+    ) -> impl Stream<Item = Result<(u128, Order), Self>> + '_ {
         ch_orders::query(self, package, ch, version, asks)
     }
 
@@ -53,29 +53,25 @@ pub trait GraphQlClientExt: GraphQlClient + Sized {
         &self,
         map: ObjectId,
         ch_version: Option<Version>,
-    ) -> impl Stream<Item = Result<(u128, Order), Error<Self::Error>>> + '_ {
+    ) -> impl Stream<Item = Result<(u128, Order), Self>> + '_ {
         map_orders::query(self, map, ch_version)
     }
 
     /// Object IDs of the orderbook and asks/bids maps for a market.
     ///
     /// These never change, so you can query them once and save them.
-    fn order_maps(
-        &self,
-        package: Address,
-        ch: ObjectId,
-    ) -> impl Future<Output = Result<OrderMaps, Error<Self::Error>>> + Send {
+    async fn order_maps(&self, package: Address, ch: ObjectId) -> Result<OrderMaps, Self> {
         order_maps::query(self, package, ch)
     }
 
     /// The unparsed clearing house's collateral [`Vault`].
     ///
     /// [`Vault`]: crate::Vault
-    fn clearing_house_vault(
+    async fn clearing_house_vault(
         &self,
         package: Address,
         ch: ObjectId,
-    ) -> impl Future<Output = Result<MoveInstance<Vault>, ChVaultError<Self::Error>>> + Send {
+    ) -> ::std::result::Result<MoveInstance<Vault>, ChVaultError<Self::Error>> {
         ch_vault::query(self, package, ch)
     }
 
@@ -86,7 +82,7 @@ pub trait GraphQlClientExt: GraphQlClient + Sized {
         &self,
         ch: ObjectId,
         version: Option<Version>,
-    ) -> impl Stream<Item = Result<(u64, MoveInstance<Position>), Error<Self::Error>>> + '_ {
+    ) -> impl Stream<Item = Result<(u64, MoveInstance<Position>), Self>> + '_ {
         ch_positions::query(self, ch, version)
     }
 }
