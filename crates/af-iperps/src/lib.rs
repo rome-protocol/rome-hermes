36ea27f06629149bf021b22065e1309135bd197f
matteo-aftermath <matteo@aftermath.finance>
2025-07-30 02:39:14 +0200

    tests(af-iperps): temporarily remove public_api test

--- crates/af-iperps/src/lib.rs
+++ crates/af-iperps/src/lib.rs
@@ -1016,30 +1016,30 @@ impl self::ordered_map::Map<Order> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    /// Taken from
-    /// <https://github.com/cargo-public-api/cargo-public-api?tab=readme-ov-file#-as-a-ci-check>
-    #[test]
-    fn public_api() {
-        // Install a compatible nightly toolchain if it is missing
-        rustup_toolchain::install(public_api::MINIMUM_NIGHTLY_RUST_VERSION).unwrap();
+// #[cfg(test)]
+// mod tests {
+//     /// Taken from
+//     /// <https://github.com/cargo-public-api/cargo-public-api?tab=readme-ov-file#-as-a-ci-check>
+//     #[test]
+//     fn public_api() {
+//         // Install a compatible nightly toolchain if it is missing
+//         rustup_toolchain::install(public_api::MINIMUM_NIGHTLY_RUST_VERSION).unwrap();
 
-        // Build rustdoc JSON
-        let rustdoc_json = rustdoc_json::Builder::default()
-            .toolchain(public_api::MINIMUM_NIGHTLY_RUST_VERSION)
-            .build()
-            .unwrap();
+//         // Build rustdoc JSON
+//         let rustdoc_json = rustdoc_json::Builder::default()
+//             .toolchain(public_api::MINIMUM_NIGHTLY_RUST_VERSION)
+//             .build()
+//             .unwrap();
 
-        // Derive the public API from the rustdoc JSON
-        let public_api = public_api::Builder::from_rustdoc_json(rustdoc_json)
-            .omit_blanket_impls(true)
-            .omit_auto_trait_impls(true)
-            .omit_auto_derived_impls(true)
-            .build()
-            .unwrap();
+//         // Derive the public API from the rustdoc JSON
+//         let public_api = public_api::Builder::from_rustdoc_json(rustdoc_json)
+//             .omit_blanket_impls(true)
+//             .omit_auto_trait_impls(true)
+//             .omit_auto_derived_impls(true)
+//             .build()
+//             .unwrap();
 
-        // Assert that the public API looks correct
-        insta::assert_snapshot!(public_api);
-    }
-}
+//         // Assert that the public API looks correct
+//         insta::assert_snapshot!(public_api);
+//     }
+// }
